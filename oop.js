14aaac3e180e1560cda59b621aaa58f24112c99e
Abbas4d4 <Abbas.Darboe@ada.ac.uk>
2024-10-25 19:18:24 +0000

    first commit

--- oop.js
+++ oop.js
@@ -3,6 +3,7 @@ PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 function validInteger (value) { // value can be a string or a number (integer)
   
+  return Number.isInteger(value) && value > 0;
 }  
 
 
@@ -12,17 +13,35 @@ function validatePriority(priority) { // value can be a string or a number (inte
 
 
 function todaysDate () {
+  // Returns the current date and time in the format 'dd/mm/yyyy hh:mm:ss'.
   
 }
 
 
 class Task  {
+  _added
+  _title
+  _priority
+
+  constructor(added,title,priority){
+    this._added = added
+    this._title = title
+    this._priority = priority
+  }
 
   // (title, priority)
 }
 
 
 class ToDo {
+  constructor(){
+    this.tasks = ['']
+  }
+
+  add(){}
+  remove(){}
+  list(){}
+  task(){}
     
 }
 
